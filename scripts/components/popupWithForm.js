f6a12d431ed63ebb2270e8f6f7c6a8b254bef5da
arty gvozdenkov <gvozdenkov@gmail.com>
2022-07-08 17:00:32 +0300

    feat: add popupWithForm class for edit profile and add card

---
+++ scripts/components/popupWithForm.js
@@ -0,0 +1,34 @@
+import { formInputSelector, formSelector } from "../utils/constants.js";
+import Popup from "./popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
+    this._form = this._popup.querySelector(formSelector);
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  _getInputValues() {
+    this._formValues = {};
+    this._inputList = this._form.querySelectorAll(formInputSelector);
+    this._inputList.forEach(
+      (input) => (this._formValues[input.name] = input.value)
+    );
+
+    return this._formValues;
+  }
+
+  close() {
+    this._form.reset();
+    super.close();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+      this.close();
+    });
+  }
+}
